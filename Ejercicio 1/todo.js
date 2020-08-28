8d4a7d08088b137bf4d7e6d223962d3e5a2347df
Santiago <sjlarrain@uc.cl>
2020-08-28 18:55:06 -0400

    Ejercicio 01

--- Ejercicio 1/todo.js
+++ Ejercicio 1/todo.js
@@ -4,9 +4,20 @@ const list = document.getElementById("list")
 
 
 button.addEventListener("click", () => {
-    const parrafo = document.createElement("p")
-    parrafo.textContent = input.value
-    list.appendChild(parrafo)
+    const parrafo = document.createElement("p");
+    parrafo.textContent = input.value;
+    list.appendChild(parrafo);
 
 })
 
+input.addEventListener("change", () => {
+    console.log(input.value)
+    if (input.value == ""){
+        console.log(input.value)
+        button.disabled = true;
+    }
+    else {
+        console.log(input.value)
+        button.disabled = false;
+    }
+})
