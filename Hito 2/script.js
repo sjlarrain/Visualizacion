5cf21cace2d35c54a71851a9147838af35738186
Santiago <sjlarrain@uc.cl>
2020-10-11 17:09:45 -0300

    Path :panda_face:

--- Hito 2/script.js
+++ Hito 2/script.js
@@ -24,9 +24,19 @@ const promedio = (datos) => {
 } 
 
 const tarjeta = (player) => {
+    const size = {HEIGHT: 600,
+                  WIDTH: 600}
+    const margin = {left: 100,
+                    right: 100,
+                    top: 70,
+                    bottom: 70}
     const texto = [player.name, player.club, player.league, player.rating]
     const posicion = [[100,20], [100,40], [100,60],[20,20]]
-    const tamanho = ["0.5 cm", "0.3 cm", "0.3 cm", "0.5 cm"]
+    const tamanho = ["0.7cm", "0.5cm", "0.5cm", "0.7cm"]
+    const centros = {x: (size.WIDTH - margin.left - margin.right) / 2, 
+                y: (size.HEIGHT - margin.top -margin.bottom) / 2 + 50 }
+    
+
     const color = (player) => {
         if (player.rating >= 75) {
             return "gold"
@@ -37,12 +47,7 @@ const tarjeta = (player) => {
         }
     }
 
-    const size = {HEIGHT: 600,
-                  WIDTH: 600}
-    const margin = {left: 100,
-                    right: 100,
-                    top: 70,
-                    bottom: 70}
+
     const container = d3.select(".container")
                         .append("svg")
                         .attr("class", "svg")
@@ -61,9 +66,41 @@ const tarjeta = (player) => {
              .style("font-size", (_, i) => tamanho[i])
              .text((d) => d)
              .attr("x", (_,i) => posicion[i][0])
-             .attr("y", (_,i)=> posicion[i][1])
+             .attr("y", (_,i)=> posicion[i][1] + 30)
              .attr("transform", `translate (${margin.left} ${margin.top})`)
-             
+    
+    container.append("circle")
+            .attr("cy",  centros.y )
+            .attr("cx",  centros.x )
+            .attr("transform", `translate (${margin.left} ${margin.top})`)
+            .attr("r", 120)
+            .attr("fill", "white")
+
+    const arc =  d3.line()
+                    .x((d) => d.x)
+                    .y((d) => d.y)
+
+    const rScale = d3.scaleLinear()
+                        . domain([0, 100])
+                        .range([0, 120])
+    
+    const lines = [{"x":rScale(player.defending)  + centros.x, "y": centros.y},
+                    {"x":centros.x, "y": rScale(player.shooting) + centros.y}, 
+                    {"x": centros.x - rScale(player.pace), "y":centros.y},
+                    {"x":centros.x, "y": centros.y - rScale(player.passing)},
+                    {"x":rScale(player.defending)  + centros.x, "y": centros.y} ] //{"x":, "y":}, {"x":, "y":}
+   
+    // const lines = [{"x":centros.x + 10,"y": centros.y + 10},
+    //                 {"x":centros.x + 10,"y": centros.y - 10},
+    //                 {"x":centros.x - 10,"y": centros.y + 10},
+    //                 {"x":centros.x - 10,"y": centros.y - 10}]
+   
+    container.append("path")
+                .attr("d", arc(lines))
+                .attr("transform", `translate (${margin.left} ${margin.top})`)
+                .attr("stroke", "blue")
+                .attr("fill", "white")
+        
 }
 const interpreter = (d) => {
     return { name: d.NAME,
