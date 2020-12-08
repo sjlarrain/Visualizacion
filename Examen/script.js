1589f84b6d68d37cbd8784cc6c47ccb8b6e37858
Santiago <sjlarrain@uc.cl>
2020-12-08 15:33:39 -0300

    American Leagues :baseball: :football: : basketball:

--- Examen/script.js
+++ Examen/script.js
@@ -10,19 +10,38 @@ const svg = d3.selectAll(".usa")
               .attr("height", size.height);
 
 const g = svg.append("g")
+const nba = svg.append("g")
+const nfl = svg.append("g")
+const mlb = svg.append("g")
 
 const interpreter = (d) => {
-    return {ID: parseInt(d.ID),
-        AB:d.ABBREVIATION,
+    return {AB:d.ABBREVIATION,
         NAME: d.NAME,
         CITY: d.CITY,
         CONFERENCE: d.CONFERENCE,
-        DIVISION: d.DIVISION
+        DIVISION: d.DIVISION,
+        longitude: parseFloat(d.longitude),
+        latitude: parseFloat(d.latitude)
         }
 };
 
 
-d3.json("usa.geojson").then((datos) => {
+
+const locations = (ciudad) => {
+    console.log(ciudad)
+    d3.json("cities_keys.json").then((datos)=>{
+        console.log([datos[ciudad].longitude, datos[ciudad].latitude])
+        const lista = [datos[ciudad].longitude, datos[ciudad].latitude]
+        return lista
+    })
+    .catch((err) => {
+        console.log("Error")
+        console.log(err)
+        return [datos[0].longitude, datos[0].latitude]
+    })
+}
+
+d3.json("json_locations/usa.geojson").then((datos) => {
     const proyeccion = d3.geoMercator().fitSize([size.width, size.height], datos);
     const caminoGeo = d3.geoPath().projection(proyeccion);
     
@@ -36,15 +55,46 @@ d3.json("usa.geojson").then((datos) => {
         .attr("opacity", 0.3)
         .attr("stroke", "grey")
         
-
+    d3.csv("csv_locations/mlb_teams_loc.csv", interpreter).then((datos)=>{
+        console.log(datos)
+        mlb.selectAll("circle")
+            .data(datos)
+            .enter()
+            .append("circle")
+            .attr("cx", (d) => proyeccion([d.longitude, d.latitude])[0])
+            .attr("cy", (d) => proyeccion([d.longitude, d.latitude])[1])
+            .attr("r", 3)
+            .attr("fill", "red")
+    })
+    d3.csv("csv_locations/nfl_teams_loc.csv", interpreter).then((datos)=>{
+        console.log(datos)
+        nfl.selectAll("circle")
+            .data(datos)
+            .enter()
+            .append("circle")
+            .attr("cx", (d) => proyeccion([d.longitude, d.latitude])[0])
+            .attr("cy", (d) => proyeccion([d.longitude, d.latitude])[1])
+            .attr("r", 3)
+            .attr("fill", "green")
+    })
+    d3.csv("csv_locations/nba_teams_loc.csv", interpreter).then((datos)=>{
+        console.log(datos)
+        nba.selectAll("circle")
+            .data(datos)
+            .enter()
+            .append("circle")
+            .attr("cx", (d) => proyeccion([d.longitude, d.latitude])[0])
+            .attr("cy", (d) => proyeccion([d.longitude, d.latitude])[1])
+            .attr("r", 3)
+            .attr("fill", "blue")
+    })
     
     const driverZoom = (evento) => {
         const t = evento.transform; 
         g.attr("transform", t);
 
     }
-        
-    
+
     const zoom = d3.zoom()
     .extent([[0, 0], [size.width, size.height]])
     .translateExtent([[0, 0], [size.width, size.height]])
