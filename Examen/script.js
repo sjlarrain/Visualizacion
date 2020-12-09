edc8941aa0fa85ae74477ef8f983fd75e2e29c23
Santiago <sjlarrain@uc.cl>
2020-12-08 23:57:15 -0300

    Start :baseball::basketball::football:

--- Examen/script.js
+++ Examen/script.js
@@ -1,9 +1,11 @@
+
+// Mesures 
 const size = {
     width: 800,
     height:600
 };
 const sizeTree = {
-    width: 1500,
+    width: 800,
     height:500
 };
 
@@ -14,23 +16,27 @@ const margin = {
     right: 50,
   };
 
+
+// SVG & "g" generations  
 const svg = d3.selectAll(".usa")
               .append("svg")
               .attr("class", 'svg')
               .attr("width", size.width)
-              .attr("height", size.height);
-
-const g = svg.append("g")
-const nba = g.append("g")
-const nfl = g.append("g")
-const mlb = g.append("g")
+              .attr("height", size.height)
+              .style("border", "1px solid black");
+const h = svg.append("g")
+const g = h.append("g")
+const nba = h.append("g")
+const nfl = h.append("g")
+const mlb = h.append("g")
 
 const mlbTree_svg = d3.selectAll(".mlb_tree")
                     .append("svg")
                     .attr("class", 'svg')
                     .attr("width", sizeTree.width)
                     .attr("height", sizeTree.height)
-
+                    .attr("padding", 0)
+                    .style("border", "1px solid black");
 const mlbTree = mlbTree_svg.append("g")
                     .attr("width", sizeTree.width - margin.left - margin.right)
                     .attr("height", sizeTree.height - margin.top - margin.bottom)
@@ -40,7 +46,7 @@ const nbaTree_svg = d3.selectAll(".nba_tree")
                     .attr("class", 'svg')
                     .attr("width", sizeTree.width)
                     .attr("height", sizeTree.height)
-
+                    .style("border", "1px solid black");
 const nbaTree = nbaTree_svg.append("g")
                     .attr("width", sizeTree.width - margin.left - margin.right)
                     .attr("height", sizeTree.height - margin.top - margin.bottom)
@@ -50,14 +56,14 @@ const nflTree_svg = d3.selectAll(".nfl_tree")
                     .attr("class", 'svg')
                     .attr("width", sizeTree.width)
                     .attr("height", sizeTree.height)
-
+                    .style("border", "1px solid black");
 const nflTree = nflTree_svg.append("g")
                     .attr("width", sizeTree.width - margin.left - margin.right)
                     .attr("height", sizeTree.height - margin.top - margin.bottom)
 
 
 
-
+// functions
 const interpreter = (d) => {
     return {AB:d.ABBREVIATION,
         NAME: d.NAME,
@@ -121,6 +127,18 @@ const linksGenerator = d3
 
 }
 
+function mouseover() {
+    const clase = this.className.baseVal
+    console.log(clase)
+    d3.select(this)
+        .attr("r", 10);
+  }
+function mouseout() {
+    const clase = this.className.baseVal
+    console.log(clase)
+    d3.select(this).attr("r", 3);
+}
+// Jsons
 d3.json("json_locations/usa.geojson").then((datos) => {
     const proyeccion = d3.geoMercator().fitSize([size.width, size.height], datos);
     const caminoGeo = d3.geoPath().projection(proyeccion);
@@ -146,6 +164,8 @@ d3.json("json_locations/usa.geojson").then((datos) => {
             .attr("cy", (d) => proyeccion([d.longitude, d.latitude])[1])
             .attr("r", 3)
             .attr("fill", "red")
+            .on("mouseover", mouseover)
+            .on("mouseout", mouseout)
     })
     d3.csv("csv_locations/nfl_teams_loc.csv", interpreter).then((datos)=>{
         console.log(datos)
@@ -159,6 +179,8 @@ d3.json("json_locations/usa.geojson").then((datos) => {
             .attr("r", 3)
             .attr("fill", "green")
             .attr("transform", "translate (6 0)")
+            .on("mouseover", mouseover)
+            .on("mouseout", mouseout)
     })
     d3.csv("csv_locations/nba_teams_loc.csv", interpreter).then((datos)=>{
         console.log(datos)
@@ -172,12 +194,15 @@ d3.json("json_locations/usa.geojson").then((datos) => {
             .attr("r", 3)
             .attr("fill", "blue")
             .attr("transform", "translate (-6 0)")
+            .on("mouseover", mouseover)
+            .on("mouseout", mouseout)
     })
         
     
     const driverZoom = (evento) => {
         const t = evento.transform; 
-        g.attr("transform", t)
+        h.attr("transform", t)
+
 
     }
 
@@ -233,7 +258,9 @@ treeBuilder(raiz, nbaTree)
 console.log(error);
 });
     
-
+d3.selectAll("circle")
+    .on("mouseover", mouseover)
+    .on("mouseout", mouseout)
 
  
 
